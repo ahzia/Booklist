8dbb3010d73f3ea87da9098e310bc1f47e5ac630
codecaiine <akabrouyannickn@gmail.com>
2021-08-16 15:36:21 +0000

    Add html javascript  and  linters

---
+++ main.js
@@ -0,0 +1,13 @@
+let book = [{
+        title: 'book1',
+        author: 'author1'
+    },
+    {
+        title: 'book2',
+        author: 'author2'
+    },
+    {
+        title: 'book3',
+        author: 'author3'
+    },
+]
