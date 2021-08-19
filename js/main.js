16117f2ffabd79345f2931d5f67273f8a20b8e4a
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-08-19 15:52:36 +0430

    work on navigation

--- js/main.js
+++ js/main.js
@@ -11,3 +11,30 @@ document.addEventListener('DOMContentLoaded', () => {
     NewBooks.addBook();
   });
 });
+
+const clear = () => {
+  const menuElements = document.querySelectorAll('[data-tab]');
+  let i;
+  for (i = 0; i < menuElements.length; i += 1) {
+    menuElements[i].classList.remove('active-tab');
+    const id = menuElements[i].getAttribute('data-tab');
+    document.getElementById(id).classList.remove('active-tab');
+  }
+};
+
+const change = (e) => {
+  clear();
+  e.target.classList.add('active-tab');
+  const id = e.currentTarget.getAttribute('data-tab');
+  document.getElementById(id).classList.add('active-tab');
+};
+
+const bindAll = () => {
+  const menuElements = document.querySelectorAll('[data-tab]');
+  let i;
+  for (i = 0; i < menuElements.length; i += 1) {
+    menuElements[i].addEventListener('click', change, false);
+  }
+};
+
+bindAll();
